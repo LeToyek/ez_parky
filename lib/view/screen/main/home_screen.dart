f81781bb5dc9f5299a4195299343335e692adc2b
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-06-02 16:37:25 +0700

    feat: init main screen

---
+++ lib/view/screen/main/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:ez_parky/view/layouts/index.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return EzScaffold(
+      isDark: true,
+      title: "Home",
+      ezBody: Container(),
+    );
+  }
+}
---
+++ lib/view/screen/main/profile_screen.dart
@@ -0,0 +1,12 @@
+import 'package:ez_parky/view/layouts/index.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class EzProfileScreen extends ConsumerWidget {
+  const EzProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return EzScaffold(title: 'Profile', isDark: false, ezBody: Container());
+  }
+}
