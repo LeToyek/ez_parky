9088adfc7469002133bfb4334a983da4305975bc
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-06-08 13:10:41 +0700

    feat: init qrcode

--- lib/view/screen/main/home_screen.dart
+++ lib/view/screen/main/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:ez_parky/view/layouts/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -9,7 +10,14 @@ class HomeScreen extends ConsumerWidget {
     return EzScaffold(
       isDark: true,
       title: "Home",
-      ezBody: Container(),
+      ezBody: Container(
+          child: Center(
+              child: QrImage(
+        data: "asodkasodowqoejoqwj",
+        version: QrVersions.auto,
+        size: 400,
+        embeddedImage: Image.asset("lib/assets/logo.png").image,
+      ))),
     );
   }
 }
