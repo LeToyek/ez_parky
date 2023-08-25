4bcf49945c1e5f5bba2913fb6abc62df0289dff1
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-08-25 16:22:20 +0700

    feat: config scanner

--- lib/view/screen/main/scanner_screen.dart
+++ lib/view/screen/main/scanner_screen.dart
@@ -6,13 +6,36 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScannerScreen extends ConsumerWidget {
+class ScannerScreen extends ConsumerStatefulWidget {
   const ScannerScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends ConsumerState<ScannerScreen> {
+  late MobileScannerController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = MobileScannerController(
+      detectionSpeed: DetectionSpeed.noDuplicates,
+      facing: CameraFacing.back,
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final scannerNotifier = ref.read(scannerProvider.notifier);
-    final scanner = ref.watch(scannerProvider);
     // final isSubmitting = ref.watch(globalProvider);
     return EzScaffold(
         title: 'Parkir',
@@ -26,16 +49,20 @@ class ScannerScreen extends ConsumerWidget {
                 height: 300,
                 width: 300,
                 child: MobileScanner(
-                    controller: MobileScannerController(
-                      facing: CameraFacing.back,
-                    ),
+                    controller: controller,
+                    key: const Key("scanner"),
                     fit: BoxFit.cover,
+                    onScannerStarted: (arguments) {
+                      print("scanner started");
+                    },
+                    startDelay: true,
                     onDetect: (capture) async {
                       try {
                         Barcode barcode = capture.barcodes.first;
                         print("data barcode: ${barcode.rawValue!}");
                         await scannerNotifier.getParkingGate(barcode.rawValue!);
                         if (context.mounted) {
+                          controller.stop();
                           context.push(ParkingScreen.routePath);
                         }
                       } catch (e) {
